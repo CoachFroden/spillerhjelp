335944b9d4b0110e50bbc1b905f1648f55426449
CoachFroden <frode@vibliz.com>
2026-04-21 14:41:51 +0200

    Add files via upload

--- minside.js
+++ minside.js
@@ -1,17 +1,32 @@
 "use strict";
 
 import { auth, db } from "./firebase-refleksjon.js";
-import { collection, query, where, getDocs } 
+import { collection, query, where, getDocs, doc, setDoc, serverTimestamp } 
 from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
 
 import { onAuthStateChanged } 
 from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
+
+async function updateLastActive(user) {
+  if (!user) return;
+
+  await setDoc(
+    doc(db, "users", user.uid),
+    {
+      lastActiveAt: serverTimestamp()
+    },
+    { merge: true }
+  );
+}
 
 onAuthStateChanged(auth, async (user) => {
   if (!user) {
     window.location.href = "index.html";
     return;
   }
+
+  // 🔥 LEGG INN DENNE
+  updateLastActive(user);
 
   const q = query(
     collection(db, "spillere"),
